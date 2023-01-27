bfb129f5aa3ada3a5bf11ad3ade54b6bc93a295d
Devin Santamaria <devin@webprism.co>
2023-01-27 17:58:08 +0000

    notion client add

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,3 +1,24 @@
+import { json } from "@remix-run/node"; // or cloudflare/deno
+// import { Client } from "@notionhq/client"
+
+// export const loader = async () => {
+//   const notion = new Client({ auth: process.env.NOTION_KEY })
+  
+//   const blogDb = await notion.databases.query({
+//   database_id: process.env.DATABASE_ID!,
+//     filter: {
+//       property: "Status",
+//       status: {
+//         equals: "Posted",
+//       },
+//     },
+//   })
+  
+//   console.log(blogDb)
+  
+//   return json({ data: blogDb });
+// };
+
 export default function Blog() {
   return <main>Blog is forthcoming.</main>;
 }
