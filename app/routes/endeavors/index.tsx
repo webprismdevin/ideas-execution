7cd836c15e677e02ce594a7ca640bd8a46454ec2
Devin Santamaria <devin@webprism.co>
2023-02-01 03:26:05 +0000

    change /work to /endeavors

---
+++ app/routes/endeavors/index.tsx
@@ -0,0 +1,30 @@
+import { json } from "@remix-run/node"; // or cloudflare/deno
+import { Client } from "@notionhq/client";
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  const notion = new Client({
+    auth: process.env.NOTION_KEY,
+  });
+
+  const workDb = await notion.databases.query({
+    database_id: "edb044870040442c99241b209cde7b5b",
+    filter: {
+      property: "Status",
+      status: {
+        equals: "Ready",
+      },
+    },
+  });
+
+  return json({ posts: workDb.results });
+};
+
+export default function Work() {
+  return (
+    <main>
+      <h1>Career, Consulting & Company</h1>
+      <p>I'm going to try to detail all the stuff I've done.</p>
+    </main>
+  );
+}
