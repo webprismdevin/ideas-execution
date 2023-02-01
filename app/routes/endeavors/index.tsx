8a19a5bf9d9492476cae7a6c366a01ce13d20566
Devin Santamaria <devin@webprism.co>
2023-02-01 04:07:54 +0000

    Update 3 files

--- app/routes/endeavors/index.tsx
+++ app/routes/endeavors/index.tsx
@@ -1,6 +1,7 @@
 import { json } from "@remix-run/node"; // or cloudflare/deno
 import { Client } from "@notionhq/client";
-import { Link, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
+import styles from "~/styles/endeavors.css";
 
 export const loader = async () => {
   const notion = new Client({
@@ -17,18 +18,35 @@ export const loader = async () => {
     },
   });
 
-  return json({ posts: workDb.results });
+  return json({ work: workDb.results });
 };
 
 export default function Endeavors() {
-  const data = useLoaderData<typeof loader>();
+  const { work } = useLoaderData<typeof loader>();
 
-  console.log(data);
+  console.log(work);
 
   return (
     <main>
       <h1>Career, Consulting & Company</h1>
       <p>I'm going to try to detail all the stuff I've done.</p>
+      <hr />
+      {work.map((item: any) => (
+        <ul key={item.id}>
+          <li>
+            <h2>{item.properties.Name.title[0].plain_text}</h2>
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+              {item.properties.Tags.multi_select.map((tag: any) => (
+                <code style={{ background: tag.color }}>{tag.name}</code>
+              ))}
+            </div>
+          </li>
+        </ul>
+      ))}
     </main>
   );
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
 }
