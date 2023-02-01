d2dff7fd485c22c1723d1f9bef994e2dee5ded35
Devin Santamaria <devin@webprism.co>
2023-02-01 17:24:10 +0000

    update 4 files

--- app/routes/endeavors/index.tsx
+++ app/routes/endeavors/index.tsx
@@ -37,7 +37,16 @@ export default function Endeavors() {
             <h2>{item.properties.Name.title[0].plain_text}</h2>
             <div style={{ display: "flex", flexWrap: "wrap" }}>
               {item.properties.Tags.multi_select.map((tag: any) => (
-                <code style={{ background: tag.color }}>{tag.name}</code>
+                <code
+                  style={{
+                    background: tag.color,
+                    marginRight: 8,
+                    padding: "0px 4px",
+                    borderRadius: 2,
+                  }}
+                >
+                  {tag.name}
+                </code>
               ))}
             </div>
           </li>
