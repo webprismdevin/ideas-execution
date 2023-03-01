227b47bfc2acf508ad5845297e35d720ca02dc49
Devin Santamaria <devin@webprism.co>
2023-03-01 12:23:38 -0800

    read-more positioning fix

--- app/components/post.tsx
+++ app/components/post.tsx
@@ -11,27 +11,35 @@ export default function Post({ post }: { post: PostType }) {
   const { slug, title, short_description } = post;
 
   return (
-    <Link to={`/essays/${slug}`}>
+    <Link
+      to={`/essays/${slug}`}
+      style={{
+        border: "1px solid rgba(245, 245, 245)",
+      }}
+    >
       <div
         style={{
-          border: "1px solid rgba(245, 245, 245)",
           padding: "0.75rem 2rem",
           borderRadius: 2,
           height: "100%",
         }}
       >
         <h2>{title}</h2>
-        <p className="line_clamp">{short_description}</p>
+        <p className="line_clamp" style={{ marginBottom: '2rem' }}>{short_description}</p>
         <div
           style={{
             display: "flex",
             alignItems: "center",
             justifyContent: "flex-end",
             color: "var(--brand_dark)",
-            width: "100%"
+            position: "absolute",
+            bottom: '1rem',
+            right: '1rem'
           }}
         >
-          <div><span>Read</span></div>
+          <div>
+            <span>Read</span>
+          </div>
           <img src={ArrowRight} height={18} width={18} />
         </div>
       </div>
